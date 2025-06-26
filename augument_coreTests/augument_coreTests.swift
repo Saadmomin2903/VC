d1a9608f0b93fda28e2737f01f86e571b1b51d01
Saadmomin2903 <saadmomin5555@gmail.com>
2025-06-26 23:37:50 +0530

    route

---
+++ augument_coreTests/augument_coreTests.swift
@@ -0,0 +1,17 @@
+//
+//  augument_coreTests.swift
+//  augument_coreTests
+//
+//  Created by Saad Barkatullah Momin on 26/06/25.
+//
+
+import Testing
+@testable import augument_core
+
+struct augument_coreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
